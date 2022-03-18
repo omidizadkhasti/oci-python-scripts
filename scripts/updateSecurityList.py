2009e90fe8dc9ad48000acf2c5b52ad4f3812efd
Omid Izadkhasti <79064307+omidizadkhasti@users.noreply.github.com>
2022-03-18 13:29:05 +1100

    Update updateSecurityList.py

--- scripts/updateSecurityList.py
+++ scripts/updateSecurityList.py
@@ -4,7 +4,7 @@ config = oci.config.from_file('/etc/oci/config','eu-frankfurt-1')
 
 core_client = oci.core.VirtualNetworkClient(config)
 
-update_vcn_response = core_client.update_ssecurity_list(
+update_vcn_response = core_client.update_security_list(
     seclist_id="",
     update_security_list_details=oci.core.models.UpdateSecurityListtDetails(
         display_name="db-nonprod-subnet"))
